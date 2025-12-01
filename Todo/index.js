d835d2e92b9674a17b7affef7eb527246be29cf1
John Sang <johnsang1970@gmail.com>
2025-12-01 18:20:22 -0500

    Fixed submodule issues and tracked todo files

---
+++ Todo/index.js
@@ -0,0 +1,42 @@
+const input = document.querySelector('input[type="text"]');
+const section = document.querySelector('section');
+const ul = section.querySelector('ul');
+const addButton = document.querySelector('#addBtn');
+const deleteButton = document.querySelector('#deleteBtn');
+const clearButton = document.querySelector('#clearBtn');
+
+addButton.addEventListener('click', ()=> {
+const content = document.createElement('li');
+content.textContent = input.value;
+    if (input.value.trim() !== ''){
+        ul.appendChild(content);
+        input.value = '';
+    }
+})
+
+input.addEventListener('keypress', (e) => {
+    if (e.key === 'Enter'){
+        const content = document.createElement('li');   
+        content.textContent = input.value;
+
+        if (input.value.trim() !== ''){
+            ul.appendChild(content);
+            input.value = '';
+        }
+    }
+})
+
+  
+deleteButton.addEventListener('click', ()=> {
+const lastInput = ul.lastElementChild;
+
+if(lastInput){
+    ul.removeChild(lastInput);
+    }
+})
+
+clearButton.addEventListener('click', ()=>{
+   document.querySelector('ul').innerHTML = "";
+
+})
+
