75823d0d38e88e9a8f766400037cbc78e27c05d6
JohnSang16 <johnsang1970@gmail.com>
2025-12-03 17:36:39 -0500

    check system done

--- index.js
+++ index.js
@@ -12,7 +12,11 @@ todoForm.addEventListener("submit", function(e){
 function addTodo(){
     const output = textInput.value.trim();
     if(output.length > 0){
-        allTodos.push(output);
+        const todoObject = {
+            text: output,
+            completed: false
+        }
+        allTodos.push(todoObject);
         updateTodoList();
         saveTodos();
         textInput.value = "";
@@ -30,6 +34,7 @@ function updateTodoList(){
 function createTodoItem(todo, todoIndex){
     const todoId = "todo-"+todoIndex;
     const todoLi = document.createElement("li");
+    const todoText = todo.text;
     todoLi.className = "todo";
     todoLi.innerHTML = `
                 <input type="checkbox" name="cb" id="${todoId}">
@@ -37,7 +42,7 @@ function createTodoItem(todo, todoIndex){
                     <svg fill="transparent" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z"/></svg>    
                 </label>
                 <label for="${todoId}" class="todo-text">
-                    "${todo}"
+                    "${todoText}"
                 </label>
                 <button class="deletebtn">
                     <svg fill="var(--secondary-color)" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg>
@@ -46,6 +51,13 @@ function createTodoItem(todo, todoIndex){
     deletebtn.addEventListener("click", ()=>{
         deleteTodoItem(todoIndex);
     })
+
+    const checkbox = todoLi.querySelector("input");
+    checkbox.addEventListener("change", ()=>{
+        allTodos[todoIndex].completed = checkbox.checked;
+        saveTodos();
+    })
+    checkbox.checked = todo.completed;
     return todoLi;
 }
 function deleteTodoItem(todoIndex){
