88e2b91401b0f0468b04e72725122286c8272ebd
JohnSang16 <johnsang1970@gmail.com>
2025-12-03 15:53:37 -0500

    delete btn completed

--- index.js
+++ index.js
@@ -42,8 +42,16 @@ function createTodoItem(todo, todoIndex){
                 <button class="deletebtn">
                     <svg fill="var(--secondary-color)" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg>
                 </button>`;
-    
+    const deletebtn = todoLi.querySelector(".deletebtn");
+    deletebtn.addEventListener("click", ()=>{
+        deleteTodoItem(todoIndex);
+    })
     return todoLi;
+}
+function deleteTodoItem(todoIndex){
+    allTodos = allTodos.filter((_, i)=> i !== todoIndex);
+    saveTodos();
+    updateTodoList();
 }
 
 function saveTodos(){
